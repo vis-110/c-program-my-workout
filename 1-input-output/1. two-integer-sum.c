7b43e2fd8c193b99e21077c44046f6432a08c804
vis-110 <vishnuravi15898@gmail.com>
2023-07-06 11:07:13 +0530

    C input output programs

---
+++ 1-input-output/1. two-integer-sum.c
@@ -0,0 +1,21 @@
+
+// 1. Write a program that takes two integers as input and prints their sum.
+// 	Expected Output :
+// 	Sample Input: 4 7
+// 	Sample Output: 11
+#include <stdio.h>
+int main(){
+    int a,b,c;
+    printf("Please Enter any two numbers\n");
+    scanf("%d %d",&a,&b);
+    c=a+b;
+    printf("The sum is : %d",c);    
+    return 0;
+}
+// Output
+// F:\start-c-program>output
+// Please Enter any two numbers
+// 4
+// 7
+// The sum is : 11
+// F:\start-c-program>
