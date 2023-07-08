7faa19ecbacc99ed802794cdb813cf57944aaf09
Vishnu R <103335258+vis-110@users.noreply.github.com>
2023-07-08 12:59:25 +0530

    Total minutes are calculate total hours and minute

---
+++ 1-input-output/7.total.mints-to-hourandmints.c
@@ -0,0 +1,28 @@
+    // Write a program in C that takes minutes as input, and
+    // display the total number of hours and minutes.
+    // // 	Expected Output :
+    // // 	Input minutes: 546
+    // // 	9 Hours, 6 Minutes
+
+
+    // #include <stdio.h>
+    // int main(){
+    //     int a,c;
+    //     float e,b,d;
+    //     printf("Please Enter the minuts\n");
+    //     scanf("%d",&a);
+    //     e=(float)a/60;
+    //     int g =(int)e;
+    //     d=e-g;
+    //     c=d*60;
+    //     printf("%d hours, %d minuts\n",g,c);
+    //     return 0;
+    // }
+
+
+
+    // F:\start-c-program>output
+    // Please Enter the minuts
+    // 546
+    // 9 hours, 6 minuts
+
