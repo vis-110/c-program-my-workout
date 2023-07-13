b1aa48c5da640bb6e06d23cccaf51b1d5ad1169c
Vishnu R <103335258+vis-110@users.noreply.github.com>
2023-07-13 09:47:41 +0530

    Add files via upload

---
+++ 1-input-output/14.0-127-find-character.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+     int a,b,e,d;
+    printf("please enter the any four integer number 0-127\n");
+    scanf("%d %d %d %d",&a,&b,&e,&d);
+    printf("Character of %d is %c \n",a,a);
+    printf("Character of %d is %c \n",b,b);
+    printf("Character of %d is %c \n",e,e);
+    printf("Character of %d is %c \n",d,d);
+
+    return 0;
+}
