3df55d629070bd14ee3948d83398206e6ac69cb2
Vishnu R <103335258+vis-110@users.noreply.github.com>
2023-07-13 09:48:24 +0530

    Add files via upload

---
+++ 1-input-output/15.two-num-quotient.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    float a,b;
+    printf("please enter the any two number\n");
+    scanf("%f %f",&a,&b);
+    printf("Quotient of a and b is:%f\n",a/b);
+    return 0;
+}
+// F:\start-c-program>output
+// please enter the any two number
+// 32 7
+// Quotient of a and b is:4.571429
