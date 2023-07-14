fc813540310db8c8570ed75672c79e53895bb715
Vishnu R <103335258+vis-110@users.noreply.github.com>
2023-07-14 07:07:46 +0530

    Add files via upload

---
+++ 1-input-output/17.two-num-swap.c
@@ -0,0 +1,42 @@
+    //17. Write a C program to swap two integers.
+    // 	Expected Output:
+    // 	Input the a and b values: 45  31
+
+    // 	Before Swapping
+    // 	value of a is: 45
+    // 	value of b is: 31
+
+    // // 	After Swapping
+    // // 	value of a is: 31
+    // // 	value of b is: 45
+
+    // #include <stdio.h>
+    // int main()
+    // {
+    //     int a, b, temp;
+    //     printf("Enter the two values\n");
+    //     scanf("%d %d", &a, &b);
+    //     printf("Before Swapping\n");
+    //     printf("value of a is: %d\n", a);
+    //     printf("value of b is: %d\n", b);
+    //     temp = a;
+    //     a = b;
+    //     b = temp;
+    //     printf("After Swapping\n");
+    //     printf("value of a is: %d\n", a);
+    //     printf("value of b is: %d\n", b);
+    //     return 0;
+    // }
+    /*
+    Output:
+
+    Enter the two values
+    21
+    42
+    Before Swapping
+    value of a is: 21
+    value of b is: 42
+    After Swapping
+    value of a is: 42
+    value of b is: 21
+*/
