eba8640ccae531b296d3dcd4b8497a1050c9c95b
Vishnu R <103335258+vis-110@users.noreply.github.com>
2023-07-15 20:25:06 +0530

    Add files via upload

---
+++ 1-input-output/4.leap-year.c
@@ -0,0 +1,63 @@
+
+  // write a C program to find whether a given year is a leap year or not .
+
+  // #include <stdio.h>
+  //                                                                     int
+  //                                                         main()
+  // {
+  //   int given;
+  //   printf("Enter any year\n");
+  //   scanf("%d", &given);
+  //   if (((a % 4 == 0) && (a % 100 != 0)) || ((a % 400 == 0)))
+  //   {
+  //     printf("This year are leap year");
+  //   }
+  //   else
+  //   {
+
+  //     printf("This year are not leap year");
+  //   }
+  //   return 0;
+  // }
+
+  // Enter any year 
+  // 1600 
+  // This year are leap year
+
+      // #include <stdio.h>
+      // int main(){
+      //     int a,i;
+      //     printf("please Enter any year\n");
+      //     for(a=1000;a<=2000;a++){
+      //     if((a%4 == 0) && (a%100 ==0 )&&(a%400 != 0))
+      //     {
+      //         printf("%d\n",a);
+      //     }
+      //     else{
+
+      //     }
+      //     }
+      //     return 0;
+      // }
+
+      // #include <stdio.h>
+      // int main(){
+      //     int a;
+      //    // printf("please Enter any year\n");
+      //     //scanf("%d",&a);
+      //     for(int a = 1900; a <= 2000; a++)
+      //     {
+      //         if(((a%4 == 0) && (a%100 !=0 ))||((a%400 == 0) &&(a%100 ==0)))
+      //         {
+      //           //  printf("This year are leap year");
+      //           printf("%d\t",a);
+      //         }
+      //         else{
+      //           //  printf("%d\t",a);
+
+      //            // printf("This year not a leap year");
+      //         }
+      //     }
+
+      //     return 0;
+      // }
